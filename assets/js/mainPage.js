783a5869a0103c96eed697960080e7d21da35933
Iliya Soleimani <zalizadeh780@gmail.com>
2023-07-18 15:26:11 +0330

    Added Trusted by Container

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -1,15 +1,22 @@
 function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var elemTop = $(elem).offset().top;
-    return (elemTop >= docViewTop);
+  var docViewTop = $(window).scrollTop();
+  var elemTop = $(elem).offset().top;
+  return elemTop >= docViewTop;
 }
 
-const elements = document.getElementsByClassName('creative-service-container');
-const elementsAnimations = document.getElementsByClassName('creative-service-animate');
-window.addEventListener('scroll', () => {
-    for (let index = 0; index < elements.length; index++) {
-        if (isScrolledIntoView(elements[index])) {
-            elementsAnimations[index].classList.add("animate__rotateIn");
-        }
+const elements = document.getElementsByClassName("creative-service-container");
+const elementsAnimations = document.getElementsByClassName(
+  "creative-service-animate"
+);
+const trustedContainer = document.getElementById("trusted-by");
+window.addEventListener("scroll", () => {
+  for (let index = 0; index < elements.length; index++) {
+    if (isScrolledIntoView(elements[index])) {
+      elementsAnimations[index].classList.add("animate__rotateIn");
     }
-})
+  }
+
+  if (isScrolledIntoView(trustedContainer)) {
+    trustedContainer.classList.add("animate__slideInLeft");
+  }
+});
