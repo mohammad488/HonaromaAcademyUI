63f54e3b0c655816eb60e4db625e1f71977ecef8
Iliya Soleimani <zalizadeh780@gmail.com>
2023-07-19 21:15:23 +0330

    Added Job Listings

--- assets/js/jobPostsPage.js
+++ assets/js/jobPostsPage.js
@@ -3,20 +3,50 @@ var swiper = new Swiper(".imageSlider", {
   spaceBetween: 5,
   loop: true,
   autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+    delay: 2500,
+    disableOnInteraction: false,
   },
-  direction: "horizontal"
+  direction: "horizontal",
 });
 
-if(window.innerWidth > 576){
-    const detailsImage = document.getElementById("details-image");
-    detailsImage.style.opacity = "0";
-    window.addEventListener("scroll", () => {
-      if (isScrolledIntoView(detailsImage)) {
-        detailsImage.style.opacity = "100%";
-        detailsImage.classList.add("animate__fadeIn");
-      }
-    });
-    
-  }
+if (window.innerWidth > 576) {
+  const detailsImage = document.getElementById("details-image");
+  detailsImage.style.opacity = "0";
+  window.addEventListener("scroll", () => {
+    if (isScrolledIntoView(detailsImage)) {
+      detailsImage.style.opacity = "100%";
+      detailsImage.classList.add("animate__fadeIn");
+    }
+  });
+}
+if (window.innerWidth > 576) {
+  const detailsImage = document.getElementById("details-image");
+  detailsImage.style.opacity = "0";
+  window.addEventListener("scroll", () => {
+    if (isScrolledIntoView(detailsImage)) {
+      detailsImage.style.opacity = "100%";
+      detailsImage.classList.add("animate__fadeIn");
+    }
+  });
+}
+
+let jobPosts = document.querySelectorAll("div.job-post");
+
+jobPosts.forEach(function (jobPost) {
+  jobPost.addEventListener("click", function () {
+    jobPost.classList.toggle("active");
+
+    const plusIcon = jobPost.getElementsByClassName("ri-add-line")[0];
+    if (plusIcon.style.display === "none") {
+      console.log("closed!");
+      plusIcon.style.display = "block";
+      jobPost.getElementsByClassName("ri-subtract-line")[0].style.display =
+        "none";
+    } else {
+      console.log("opened!");
+      plusIcon.style.display = "none";
+      jobPost.getElementsByClassName("ri-subtract-line")[0].style.display =
+        "block";
+    }
+  });
+});
